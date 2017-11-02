14a76e5f590be3b2bfd849f1874603291e91ecc6
John McKenzie <jmckind@gmail.com>
2017-11-02 11:11:57 -0500

    Increment version number

--- src/setup.py
+++ src/setup.py
@@ -6,7 +6,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='whoosis',
-    version='0.0.13',
+    version='0.0.14',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
